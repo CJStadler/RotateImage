d5d700ecf4bd470802d5c53c834bd034bef6ff0b
Chris Stadler <chrisstadler@gmail.com>
2014-01-02 11:15:22 -0500

    Added main function and command line interface
    Does not yet rotate

--- RotateImage.py
+++ RotateImage.py
@@ -2,7 +2,26 @@
 Chris Stadler
 RotateImage.py
 """
+import Image
 
-def rotate(fileName, degrees):
-    print("rotating image")
-    return
+def main(fileName, outfile, degrees):
+    assert degrees%90 == 0 # can only rotate a multiple of 90.
+    im = Image.open(fileName)
+    w, h = im.size
+    assert w == h # must be square.
+    data = im.getdata()
+    rotateData(data, degrees)
+    im.putdata(data)
+    im.save(outfile)
+
+def rotateData(data, degrees):
+    return True
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description="Zip and unzip using Huffman coding")
+    parser.add_argument('inImage', help='Image to rotate')
+    parser.add_argument('outImage', help='Save as')
+    parser.add_argument('degrees', type=int, help='degrees to rotate, multiple of 90')
+    args = parser.parse_args()
+    main(args.inImage,args.outImage,args.degrees)
