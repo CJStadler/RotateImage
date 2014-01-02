b29ab22d9eed63a0f09063bef76902223a8861df
Chris Stadler <chrisstadler@gmail.com>
2014-01-02 12:10:07 -0500

    Finished 90 degree implementation

--- RotateImage.py
+++ RotateImage.py
@@ -1,27 +1,43 @@
 """
 Chris Stadler
 RotateImage.py
+
+Rotates a square image 90 degrees.
 """
 import Image
 
-def main(fileName, outfile, degrees):
-    assert degrees%90 == 0 # can only rotate a multiple of 90.
+def main(fileName):
     im = Image.open(fileName)
     w, h = im.size
     assert w == h # must be square.
-    data = im.getdata()
-    rotateData(data, degrees)
+    data = list(im.getdata())
+    #print len(data)
+    rotate90(data, w)
     im.putdata(data)
-    im.save(outfile)
+    im.save(fileName)
 
-def rotateData(data, degrees):
-    return True
+def rotate90(data, w):
+    row = 0
+    col = 0
+    for ring in range(w/2):
+        corner = ring*w + ring
+        rw = w - ring*2
+        for quad in range(w - 1 - ring*2):
+            q1 = corner + quad
+            q2 = corner + quad*w + rw -1
+            q3 = corner + (rw-1)*w + rw - quad -1
+            q4 = corner + (rw-quad-1)*w
+            temp = data[q1]
+            data[q1] = data[q4]
+            data[q4] = data[q3]
+            data[q3] = data[q2]
+            data[q2] = temp
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description="Zip and unzip using Huffman coding")
-    parser.add_argument('inImage', help='Image to rotate')
-    parser.add_argument('outImage', help='Save as')
-    parser.add_argument('degrees', type=int, help='degrees to rotate, multiple of 90')
+    parser.add_argument('fileName', help='Image to rotate')
+    #parser.add_argument('outImage', help='Save as')
+    #parser.add_argument('degrees', type=int, help='degrees to rotate, multiple of 90')
     args = parser.parse_args()
-    main(args.inImage,args.outImage,args.degrees)
+    main(args.fileName)
